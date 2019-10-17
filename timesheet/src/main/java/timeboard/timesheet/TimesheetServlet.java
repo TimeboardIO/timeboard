56463a54fd47e418820c24d7879e2c496402ddca
Nicolas Le Floc'h <nicolas.lefloch@mythalesgroup.io>
2019-10-17 17:57:17 +0200

    Validation button disabled when all required are not ok

--- timesheet/src/main/java/timeboard/timesheet/TimesheetServlet.java
+++ timesheet/src/main/java/timeboard/timesheet/TimesheetServlet.java
@@ -72,6 +72,20 @@ public class TimesheetServlet extends TimeboardServlet {
     }
 
 
+    private int findLastWeekYear(Calendar c, int week, int year) {
+        c.set(Calendar.YEAR , year);
+        c.set(Calendar.WEEK_OF_YEAR , week);
+        c.roll(Calendar.WEEK_OF_YEAR, -1); // remove 1 week
+        return c.get(Calendar.YEAR);
+    }
+
+    private int findLastWeek(Calendar c, int week, int year) {
+        c.set(Calendar.YEAR , year);
+        c.set(Calendar.WEEK_OF_YEAR , week);
+        c.roll(Calendar.WEEK_OF_YEAR, -1); // remove 1 week
+        return c.get(Calendar.WEEK_OF_YEAR);
+    }
+
     private Date findStartDate(Calendar c, int week, int year) {
         c.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
         return c.getTime();
@@ -100,8 +114,14 @@ public class TimesheetServlet extends TimeboardServlet {
         final Date ds = findStartDate(c, week, year);
         final Date de = findEndDate(c, week, year);
 
+
+        final int lastWeek = findLastWeek(c, week, year);
+        final int lastWeekYear = findLastWeekYear(c, week, year);
+        final boolean lastWeekValidated = this.timesheetService.isTimesheetValidated(SecurityContext.getCurrentUser(request), lastWeekYear, lastWeek);
+
         viewModel.getViewDatas().put("week", week);
         viewModel.getViewDatas().put("year", year);
+        viewModel.getViewDatas().put("lastWeekValidated", lastWeekValidated);
 
         viewModel.setTemplate("timesheet.html");
     }
--- timesheet/src/main/resources/static/timesheet.js
+++ timesheet/src/main/resources/static/timesheet.js
@@ -12,8 +12,6 @@ const updateTask = function(date, task, type, val){
     });
 }
 
-
-
 const timesheetModel = {
     week:0,
     year:0,
@@ -36,6 +34,19 @@ const timesheetModel = {
     getImputation: function(date, taskID){
         return this.imputations[date][taskID];
     },
+    enableValidateButton: function(week){
+        var result = true;
+        //check last week
+        const lastWeekValidated = $("meta[property='timesheet']").attr('lastWeekValidated');
+        result = result && lastWeekValidated;
+        //check all days imputations == 1
+        this.days.forEach(function(day) {
+        var sum = timesheetModel.getImputationSum(day.date);
+           result = result && (sum == 1);
+        });
+        console.log(result);
+        return result;
+    }
 
 }
 
@@ -45,6 +56,8 @@ $(document).ready(function(){
 
     const week = $("meta[property='timesheet']").attr('week');
     const year = $("meta[property='timesheet']").attr('year');
+    const lastWeekValidated = $("meta[property='timesheet']").attr('lastWeekValidated');
+
 
     var app = new Vue({
       el: '#timesheet',
