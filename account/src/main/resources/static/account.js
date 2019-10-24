e70f1bc54e9d43b0c6e6595347cfd8eac1b3e6f0
Nicolas Le Floc'h <nicolas.lefloch@mythalesgroup.io>
2019-10-24 15:55:12 +0200

    Front integration of account page

---
+++ account/src/main/resources/static/account.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function(){
+
+    $('.ui.form.password')
+      .form({
+        fields: {
+          oldPassword: {
+            rules: [
+              {
+                type   : 'empty',
+                prompt : 'You have to type your old password'
+              }
+            ]
+          },
+          password1: {
+            rules: [
+              {
+                type   : 'empty',
+                prompt : 'You have to specify a new password'
+              }
+            ]
+          },
+          password2: {
+            rules: [
+              {
+                type   : 'match[password1]',
+                prompt : 'Password are not the same.'
+
+              }
+            ]
+          },
+        }
+      });
+
+
+      $('.ui.form.account')
+            .form({
+              fields: {
+                email: {
+                  rules: [
+                    {
+                      type   : 'email',
+                      prompt : 'Email format is not correct'
+                    }
+                  ]
+                },
+              }
+            });
+
+});
+
+
+
+
+
+
