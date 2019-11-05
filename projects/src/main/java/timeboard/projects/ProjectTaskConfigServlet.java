2cb4dcb633739bf4a8324c004f9c2c36cc6e736e
Nicolas Le Floc'h <nicolas.lefloch.info@gmail.com>
2019-11-05 14:14:55 +0100

    Issues#53 (#75)
    * Add 1 min to date from form
    
    * Bug fixed

--- projects/src/main/java/timeboard/projects/ProjectTaskConfigServlet.java
+++ projects/src/main/java/timeboard/projects/ProjectTaskConfigServlet.java
@@ -265,8 +265,8 @@ public class ProjectTaskConfigServlet extends TimeboardServlet {
             }
             taskName = request.getParameter("taskName");
             taskComment = request.getParameter("taskComments");
-            startDate = DATE_FORMAT.parse(request.getParameter("taskStartDate"));
-            endDate = DATE_FORMAT.parse(request.getParameter("taskEndDate"));
+            startDate = new Date(DATE_FORMAT.parse(request.getParameter("taskStartDate")).getTime()+(2 * 60 * 60 * 1000) +1);
+            endDate = new Date(DATE_FORMAT.parse(request.getParameter("taskEndDate")).getTime()+(2 * 60 * 60 * 1000) +1);
             estimateWork = Double.parseDouble(request.getParameter("taskEstimateWork"));
             taskStatus = request.getParameter("taskStatus") != null ? TaskStatus.valueOf(request.getParameter("taskStatus")) : TaskStatus.PENDING;
 
