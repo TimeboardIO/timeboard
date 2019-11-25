8e3d8b4988232f51282b6c97c41cc493da8a8c0b
ClareneSzymalka <32487512+ClareneSzymalka@users.noreply.github.com>
2019-11-25 11:04:40 +0100

    Issues#137 (#141)
    * Merge EffortLeft, EffortEstimate and EffortSpent into EffortHistory
    
    * Fix merge #120 in develop

--- core/src/main/java/timeboard/core/internal/ProjectServiceImpl.java
+++ core/src/main/java/timeboard/core/internal/ProjectServiceImpl.java
@@ -621,7 +621,7 @@ public class ProjectServiceImpl implements ProjectService {
     }
 
     @Override
-    public List<EffortSpent> getEffortSpentByTaskAndPeriod(long taskId, Date startTaskDate, Date endTaskDate) {
+    public List<EffortHistory> getEffortSpentByTaskAndPeriod(long taskId, Date startTaskDate, Date endTaskDate) {
 
         return this.jpa.txExpr(entityManager -> {
             TypedQuery<Object[]> query = (TypedQuery<Object[]>) entityManager.createNativeQuery("select " +
@@ -633,13 +633,13 @@ public class ProjectServiceImpl implements ProjectService {
 
             return query.getResultList()
                     .stream()
-                    .map(x -> new EffortSpent((Date) x[0], (Double) x[1]))
+                    .map(x -> new EffortHistory((Date) x[0], (Double) x[1]))
                     .collect(Collectors.toList());
         });
     }
 
     @Override
-    public List<EffortLeft> getTaskEffortLeftHistory(long taskId) {
+    public List<EffortHistory> getTaskEffortLeftHistory(long taskId) {
         return this.jpa.txExpr(entityManager -> {
             TypedQuery<Object[]> query = (TypedQuery<Object[]>) entityManager.createNativeQuery("select " +
             "tr.revisionDate as date, tr.effortLeft as effortLeft  " +
@@ -654,7 +654,7 @@ public class ProjectServiceImpl implements ProjectService {
 
             return query.getResultList()
                     .stream()
-                    .map(x -> new EffortLeft((Date) x[0], (Double) x[1]))
+                    .map(x -> new EffortHistory((Date) x[0], (Double) x[1]))
                     .collect(Collectors.toList());
         });
     }
