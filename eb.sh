8831fc8297609c47d26e73886fd4126a8f2e6b69
Pierre MELLET <pierre.mellet@gmail.com>
2019-12-05 23:20:11 +0100

    test

---
+++ eb.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+mkdir -p build
+mkdir -p temp
+cp webapp/target/webapp-1.0-SNAPSHOT.jar temp
+cp -r scripts/aws/ temp
+cd temp && zip timeboard.$TRAVIS_COMMIT.zip -r * .[^.]* && mv *.zip ../build/
