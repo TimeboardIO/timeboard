9ad70cff7e2779eed2942a9acd508ed7ce0932fb
Pierre MELLET <pierre.mellet@gmail.com>
2019-12-06 10:18:47 +0100

    cleanup

--- eb.sh
+++ eb.sh
@@ -4,4 +4,4 @@ mkdir -p build
 mkdir -p temp
 cp webapp/target/webapp-1.0-SNAPSHOT-spring-boot.jar temp/
 cp -r scripts/aws/ temp
-cd temp && zip timeboard.$TRAVIS_COMMIT.zip -r * .[^.]* && mv *.zip ../build/
+cd temp && zip timeboard.$(git rev-parse HEAD).zip -r * .[^.]* && mv *.zip ../build/
