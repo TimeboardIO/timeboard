b7a080fa0fdb5841fb744bdc425dc23d81039d84
Pierre MELLET <pierre.mellet@gmail.com>
2019-12-06 10:14:14 +0100

    Add AWS EB ENV Var

--- eb.sh
+++ eb.sh
@@ -2,6 +2,6 @@
 
 mkdir -p build
 mkdir -p temp
-cp webapp/target/webapp-1.0-SNAPSHOT.jar temp
-cp -r scripts/aws/* temp
+cp webapp/target/webapp-1.0-SNAPSHOT-spring-boot.jar temp/
+cp -r scripts/aws/ temp
 cd temp && zip timeboard.$TRAVIS_COMMIT.zip -r * .[^.]* && mv *.zip ../build/
