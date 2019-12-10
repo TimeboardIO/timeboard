a0f4b51f46351eefdec2fe48f9d718a82ae7042f
Clarène SZYMALKA <clarene.szymalka@thalesgroup.com>
2019-12-10 18:02:58 +0100

    test

--- core/src/main/java/timeboard/core/api/exceptions/GlobalExceptionController.java
+++ core/src/main/java/timeboard/core/api/exceptions/GlobalExceptionController.java
@@ -3,9 +3,10 @@ package timeboard.core.api.exceptions;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionController {
+public class GlobalExceptionController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(CommercialException.class)
     public ModelAndView handleCommercialException(CommercialException ex) {
