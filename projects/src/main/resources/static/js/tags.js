7397d89adaa787c5b60bf2a59bea64a008cee084
Pierre MELLET <pierre.mellet@gmail.com>
2019-12-12 18:52:53 +0100

    move datatable.js to core-ui

--- projects/src/main/resources/static/js/tags.js
+++ projects/src/main/resources/static/js/tags.js
@@ -1,27 +1,6 @@
-Vue.component('data-table', {
-    props: ['config'],
-    template: `
-          <table class="ui celled table">
-              <thead>
-                  <tr>
-                      <th v-for="col in config.cols">{{col.label}}</th>
-                  </tr>
-              </thead>
-              <tbody>
-                  <tr v-for="(row, i) in config.data">
-                      <td v-for="(col, j) in config.cols">
-                          <slot :name="col.slot" v-bind:row="config.data[i]">
-                          </slot>
-                      </td>
-                  </tr>
-              </tbody>
-          </table>`
-});
-
 $(document).ready(function () {
 
     const projectID = $("meta[name='projectID']").attr('content');
-
 
     var app = new Vue({
         el: '#components-demo',
