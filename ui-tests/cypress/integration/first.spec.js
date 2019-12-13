57ed72a443ac02005ebedf9d9d22995b3cc6bb7a
Nicolas Le Floc'h <nicolas.lefloch@mythalesgroup.io>
2019-12-13 08:50:47 +0100

    First cypress test

---
+++ ui-tests/cypress/integration/first.spec.js
@@ -0,0 +1,20 @@
+describe('Post Resource', function() {
+    it('Creating a New Post', function() {
+        cy.visit('https://dev.mytimeboard.net/')     // 1.
+
+        cy.get('a.link') // 2.
+            .click()   // 3.
+
+       /* cy.get('input.post-body')  // 4.
+            .type('Hello, world!')   // 5.
+
+        cy.contains('Submit')      // 6.
+            .click()                 // 7.
+
+        cy.url()                   // 8.
+            .should('include', '/posts/my-first-post')
+
+        cy.get('h1')               // 9.
+            .should('contain', 'My First Post')*/
+    })
+})
