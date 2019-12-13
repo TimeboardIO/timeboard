db3aef9c1e5cb844937ae27abcb73112d9936502
Nicolas Le Floc'h <nicolas.lefloch@mythalesgroup.io>
2019-12-13 15:48:30 +0100

    Add project ui testing

--- ui-tests/cypress/integration/first.spec.js
+++ ui-tests/cypress/integration/first.spec.js
@@ -1,20 +1,84 @@
-describe('Post Resource', function() {
-    it('Creating a New Post', function() {
-        cy.visit('http://localhost:8080')     // 1.
+beforeEach(function () {
+    cy.visit('http://localhost:8080');
 
-        cy.get('a.link') // 2.
-            .click()   // 3.
+    cy.get('a.link')
+        .click();
 
-       /* cy.get('input.post-body')  // 4.
-            .type('Hello, world!')   // 5.
+    cy.get('input[name=username]').type("user");
 
-        cy.contains('Submit')      // 6.
-            .click()                 // 7.
+    cy.get('input[name=password]').type("password");
 
-        cy.url()                   // 8.
-            .should('include', '/posts/my-first-post')
+    cy.get('button[type=submit]')
+        .click()  ;
 
-        cy.get('h1')               // 9.
-            .should('contain', 'My First Post')*/
-    })
-})
+});
+
+
+describe('Timeboard Test', function() {
+    it('Connection user', function() {
+        cy.url().should('include', '/home')
+        cy.get('.right.menu .title').should('contain', 'user')
+    });
+
+
+    it('Timesheet Test', function() {
+        cy.contains("Timesheet")
+            .click()  ;
+    });
+
+
+});
+
+
+describe('Project Test', function() {
+
+    const projectName = 'My new project';
+
+    beforeEach(function () {
+        cy.contains("Project")
+            .click()  ;
+
+    });
+
+    it('Create project', function() {
+        cy.contains("Create Project")
+            .click();
+
+        cy.get('input[name=projectName]').type(projectName);
+
+        cy.get('button[type=submit]')
+            .click();
+
+        cy.get('.ui.card .header').should('contain', 'user');
+
+    });
+
+    it('Archive project', function() {
+        cy.get('.card').contains(projectName)
+            .get('.a').contains('Archive')
+                .click();
+
+    });
+
+    describe('Project Setup', function() {
+
+        beforeEach(function () {
+            cy.get('.card').contains('Timeboard') //projectName
+                .get('.button').contains('Setup')
+                .click();
+        });
+
+        it('Edit project config', function () {
+            cy.get('input[name=name]').clear().type(projectName);
+            cy.get('input[name=quotation]').clear().type(30000);
+            cy.get('textarea[name=comments]').clear().type('My super new project is very awesome. ');
+            cy.get('button[type=submit]')
+                .click();
+            cy.get('input[name=name]').should('be.value', projectName);
+            cy.get('input[name=quotation]').should('be.value', 30000);
+            cy.get('textarea[name=comments]').should('be', 'My super new project is very awesome. ');
+
+        });
+    });
+
+});
