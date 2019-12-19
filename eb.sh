c973221948ee8f2a3abe59e9ff0400cbffb16e7b
Pierre MELLET <pierre.mellet@gmail.com>
2019-12-19 14:45:13 +0100

    list build

--- eb.sh
+++ eb.sh
@@ -6,3 +6,4 @@ mkdir -p temp
 cp webapp/target/webapp-1.0-SNAPSHOT-spring-boot.jar temp/
 cp -r scripts/aws/ temp
 cd temp && zip timeboard.$(git rev-parse HEAD).zip -r * .[^.]* && mv *.zip ../build/
+ls -al ../build/*
