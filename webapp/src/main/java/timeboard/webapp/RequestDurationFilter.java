e07725dda0d2d5c769ce981de398834e757f5a8a
Clarène SZYMALKA <clarene.szymalka@thalesgroup.com>
2019-12-23 15:30:01 +0100

    test requestDurationFilter
    test requestDurationFilter
    
    
    test requestDurationFilter
    
    
    test requestDurationFilter
    
    
    test requestDurationFilter

---
+++ webapp/src/main/java/timeboard/webapp/RequestDurationFilter.java
@@ -0,0 +1,72 @@
+package timeboard.webapp;
+
+/*-
+ * #%L
+ * webapp
+ * %%
+ * Copyright (C) 2019 Timeboard
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.MetricRegistry;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import javax.servlet.*;
+
+@Component
+public class RequestDurationFilter implements Filter {
+
+    static final MetricRegistry metrics = new MetricRegistry();
+
+    private final Histogram responseSizes = metrics.histogram(MetricRegistry.name( "response"));
+
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+
+        long timeBefore = System.currentTimeMillis();
+        filterChain.doFilter(servletRequest, servletResponse);
+        long timeAfter = System.currentTimeMillis();
+        long duration = timeAfter - timeBefore;
+
+
+        responseSizes.update(duration);
+
+        startReport();
+
+    }
+
+
+    static void startReport() {
+        ConsoleReporter reporter = ConsoleReporter.forRegistry(metrics)
+                .convertRatesTo(TimeUnit.SECONDS)
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                .build();
+        reporter.start(1, TimeUnit.SECONDS);
+    }
+
+}
