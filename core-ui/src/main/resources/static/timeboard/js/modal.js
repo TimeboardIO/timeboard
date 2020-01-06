cbd3104ab935f81a27b354b7948e383341ab5572
Nicolas Le Floc'h <nicolas.lefloch@mythalesgroup.io>
2020-01-06 14:37:06 +0100

    Add generic confirm modal

--- core-ui/src/main/resources/static/timeboard/js/modal.js
+++ core-ui/src/main/resources/static/timeboard/js/modal.js
@@ -20,3 +20,40 @@ Vue.component('tmodal', {
 
     }
 });
+
+
+Vue.component('tmodal-confirm', {
+    props: ['title', 'id', 'text', 'icon' ],
+    template: `
+        <div class="ui basic modal">
+            <div class="ui icon header">
+                <i :class="icon" class="icon"></i>
+                {{ title }}
+            </div>
+            <div class="content">
+                <p>{{ text }}</p>
+            </div>
+            <div class="actions">
+                <div class="ui red basic cancel inverted button">
+                    <i class="remove icon"></i>
+                    No
+                </div>
+                <div class="ui green ok inverted button">
+                    <i class="checkmark icon"></i>
+                    Yes
+                </div>
+            </div>
+        </div>
+    `,
+    data: {
+        id : Math.floor(Math.random() * 10000)
+    },
+    methods: {
+        yes : function () {
+
+        },
+        no : function () {
+
+        }
+    }
+});
