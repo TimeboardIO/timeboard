3048ae175f8a0b5f169e683a6a7e348c3689ab9d
Nicolas Le Floc'h <nicolas.lefloch@mythalesgroup.io>
2020-01-06 13:28:14 +0100

    Fix can not open pending modal cols config

--- core-ui/src/main/resources/static/timeboard/js/datatable.js
+++ core-ui/src/main/resources/static/timeboard/js/datatable.js
@@ -20,7 +20,7 @@ Vue.component('data-table', {
                 </tbody>
                 <tmodal 
                     v-bind:title="'Column config '+config.name"
-                    v-bind:id="'configModal'+config.name">
+                    v-bind:id=" 'configModal' + config.name.replace(/ /g,'') ">
                     <template v-slot:content>
                         <table class="ui celled table">
                             <thead>
@@ -118,7 +118,7 @@ Vue.component('data-table', {
             this.sortOrders[key] = this.sortOrders[key] * -1
         },
         showConfigModal: function() {
-            $('#configModal'+this.config.name).modal({
+            $( '#configModal' + this.config.name.replace(/ /g, '' )).modal({
                 onApprove : function($element) {
 
                 },
@@ -145,7 +145,7 @@ Vue.component('data-table', {
                 url: "/api/datatable",
                 success: function (d) {
                     event.target.classList.toggle('loading');
-                    $('#configModal'+self.config.name).modal('hide');
+                    $('#configModal'+self.config.name.replace(/ /g, '' )).modal('hide');
                 }
             });
         }
