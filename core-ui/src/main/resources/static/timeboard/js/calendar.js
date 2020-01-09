5f94abbc47a1461bb429fdc529c4e866eadcf9d5
Nicolas Le Floc'h <nicolas.lefloch@mythalesgroup.io>
2020-01-09 17:33:34 +0100

    Create vuejs component

---
+++ core-ui/src/main/resources/static/timeboard/js/calendar.js
@@ -0,0 +1,23 @@
+Vue.component('calendar', {
+    props: ['year', 'month'],
+    template: `
+        <table class="ui celled table">
+            <tr>
+              <td data-label="10-10-10"></td>        
+            </tr>
+        </table>
+       `,
+    computed: {
+
+    },
+    methods: {
+
+
+    },
+    data : function () {
+        return {
+
+        }
+
+    }
+});
