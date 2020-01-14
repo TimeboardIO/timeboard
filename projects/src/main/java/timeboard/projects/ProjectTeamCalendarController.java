55e7039277de1143eda614defebef19f2cf5e3a2
Nicolas Le Floc'h <nicolas.lefloch@mythalesgroup.io>
2020-01-14 17:34:16 +0100

    checkstyle compliance

--- projects/src/main/java/timeboard/projects/ProjectTeamCalendarController.java
+++ projects/src/main/java/timeboard/projects/ProjectTeamCalendarController.java
@@ -32,7 +32,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import timeboard.core.api.ProjectDashboard;
 import timeboard.core.api.ProjectService;
 import timeboard.core.api.exceptions.BusinessException;
 import timeboard.core.model.Account;
