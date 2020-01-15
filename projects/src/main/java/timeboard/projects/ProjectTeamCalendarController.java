cfbca18764ad89a70fc6a0d42cdc9af27419373a
Nicolas Le Floc'h <nicolas.lefloch@mythalesgroup.io>
2020-01-15 17:16:34 +0100

    first data front import

--- projects/src/main/java/timeboard/projects/ProjectTeamCalendarController.java
+++ projects/src/main/java/timeboard/projects/ProjectTeamCalendarController.java
@@ -27,6 +27,8 @@ package timeboard.projects;
  */
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,8 +37,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import timeboard.core.api.ProjectService;
 import timeboard.core.api.exceptions.BusinessException;
 import timeboard.core.model.Account;
+import timeboard.core.model.Imputation;
 import timeboard.core.model.Project;
 import timeboard.core.security.TimeboardAuthentication;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 /**
@@ -49,6 +58,7 @@ public class ProjectTeamCalendarController {
     @Autowired
     public ProjectService projectService;
 
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
 
     @GetMapping
     protected String handleGet(TimeboardAuthentication authentication,
@@ -61,6 +71,64 @@ public class ProjectTeamCalendarController {
         model.addAttribute("project", project);
 
         return "project_calendar.html";
+    }
+
+    @GetMapping(value = "/list", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, List<CalendarEventWrapper>>> listTags(TimeboardAuthentication authentication,
+                                                                            @PathVariable Long projectID) throws BusinessException {
+        final Account actor = authentication.getDetails();
+        final Project project = this.projectService.getProjectByID(actor, authentication.getCurrentOrganization(), projectID);
+
+        List<Imputation> imputations = this.projectService.listTeamVacations(actor, project, 0);
+
+        return ResponseEntity.ok(imputations.stream()
+                .map(imputation -> new CalendarEventWrapper(imputation))
+                .collect(Collectors.groupingBy(CalendarEventWrapper::getName)));
+    }
+
+    public static class CalendarEventWrapper {
+
+
+        private String name;
+
+
+        private String date;
+        private String color;
+        private int type;
+
+        public CalendarEventWrapper() { }
+
+        public CalendarEventWrapper(Imputation imputation) {
+            this.date = DATE_FORMAT.format(imputation.getDay());
+            this.color = "lightgreen";
+            if(imputation.getValue() < 1 ){
+                this.color = "orange";
+            }
+            this.type = 1;
+            this.name = imputation.getAccount().getScreenName();
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public int getType() {
+            return type;
+        }
+
+
     }
 
 }
--- projects/src/main/resources/static/js/project_calendar.js
+++ projects/src/main/resources/static/js/project_calendar.js
@@ -1,3 +1,5 @@
+const projectID = $("meta[name='projectID']").attr('content');
+
 let app = new Vue({
     el: '#teamCalendar',
     data: {
@@ -37,6 +39,20 @@ let app = new Vue({
         ]
     },
     mounted : function () {
-
+        let self = this;
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: "projects/" + projectID + "/calendar/list",
+            success: function (d) {
+                self.teamCalendars = [];
+                for (let [key, value] of Object.entries(d)) {
+                    self.teamCalendars.push({
+                        name : key,
+                        events : value
+                    });
+                }
+            }
+        });
     }
 });
