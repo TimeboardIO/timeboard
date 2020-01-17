313a69a8128afaafb27dba11ceda014e2f027d3f
Pierre MELLET <pierre.mellet@gmail.com>
2020-01-17 07:42:18 +0100

    checkstyle

--- webapp/src/main/java/timeboard/webapp/OnboardingController.java
+++ webapp/src/main/java/timeboard/webapp/OnboardingController.java
@@ -29,9 +29,6 @@ package timeboard.webapp;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import timeboard.core.security.TimeboardAuthentication;
-
-import javax.servlet.http.HttpServletRequest;
 
 
 @Controller(OnboardingController.URI)
