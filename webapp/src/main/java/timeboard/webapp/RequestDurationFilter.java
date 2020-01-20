42b89768edb7592bbd65125e81eae7c3357b9ced
Pierre MELLET <pierre.mellet@gmail.com>
2020-01-20 14:10:33 +0100

    Issues#281 (#377)
    * test requestDurationFilter
    
    
    test requestDurationFilter
    
    
    test requestDurationFilter
    
    
    test requestDurationFilter
    
    
    test requestDurationFilter
    
    * use histogram
    
    * use slf4j
    
    
    use slf4j
    
    
    use slf4j
    
    * fix minor
    
    * request duration filter
    
    Co-authored-by: ClareneSzymalka <32487512+ClareneSzymalka@users.noreply.github.com>

---
+++ webapp/src/main/java/timeboard/webapp/RequestDurationFilter.java
@@ -0,0 +1,81 @@
+package timeboard.webapp;
+
+/*-
+ * #%L
+ * webapp
+ * %%
+ * Copyright (C) 2019 Timeboard
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+
+import com.codahale.metrics.*;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import javax.servlet.*;
+
+import static com.codahale.metrics.MetricRegistry.name;
+
+@Component
+public class RequestDurationFilter implements Filter {
+
+    @Value("${timeboard.interval.metrics.minutes}")
+    private long timeIntervalLogs;
+
+    private final String metricsMarker = "timeboard.marker.metrics";
+
+    private final MetricRegistry metrics = new MetricRegistry();
+
+    private final Timer timer = metrics.timer(name(RequestDurationFilter.class, "get-requests"));
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.startReport();
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+
+        final Timer.Context context = timer.time();
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            context.stop();
+        }
+
+    }
+
+
+    private void startReport() {
+        final Slf4jReporter reporter = Slf4jReporter.forRegistry(metrics)
+                .outputTo(LoggerFactory.getLogger(metricsMarker))
+                .convertRatesTo(TimeUnit.SECONDS)
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                .build();
+        reporter.start(this.timeIntervalLogs, TimeUnit.MINUTES);
+    }
+
+}
