01e64f47641db3c7a0a0df7cf732fba355d96b29
Pierre MELLET <pierre.mellet@gmail.com>
2020-02-17 19:59:21 +0100

    Remove Public Organization (#456)

--- core/src/main/java/timeboard/core/security/TimeboardAuthentication.java
+++ core/src/main/java/timeboard/core/security/TimeboardAuthentication.java
@@ -94,7 +94,7 @@ public class TimeboardAuthentication implements Authentication {
 
     @Transient
     public boolean currentOrganizationRole(final MembershipRole... roles) {
-        if (account == null || account.getOrganizationMemberships() == null || this.currentOrganization == null) {
+        if (account == null || account.getOrganizationMemberships() == null || currentOrganization == null) {
             return false;
         }
         return account.getOrganizationMemberships()
